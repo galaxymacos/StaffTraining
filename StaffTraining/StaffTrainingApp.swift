2df33000aec874d5dc5fcbe02ff9772cdd958c56
Xun Ruan <galaxymacos@gmail.com>
2021-12-04 04:22:54 -0500

    Initial Commit

---
+++ StaffTraining/StaffTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaffTrainingApp.swift
+//  StaffTraining
+//
+//  Created by xunruan on 2021-12-04.
+//
+
+import SwiftUI
+
+@main
+struct StaffTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
